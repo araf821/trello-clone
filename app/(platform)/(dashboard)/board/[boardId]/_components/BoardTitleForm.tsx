e019df28e3b2949f3c8e425d907e0297e06ea1c3
araf821 <araf.ahmed200@gmail.com>
2023-12-16 23:26:12 -0500

    feat: implemented board title updating

--- app/(platform)/(dashboard)/board/[boardId]/_components/BoardTitleForm.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardTitleForm.tsx
@@ -1,17 +1,33 @@
 "use client";
 
+import { updateBoard } from "@/actions/update-board";
 import { FormInput } from "@/components/form/FormInput";
 import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/useAction";
 import { Board } from "@prisma/client";
 import { ElementRef, FC, useRef, useState } from "react";
+import { toast } from "sonner";
 
 interface BoardTitleFormProps {
   board: Board;
 }
 
 const BoardTitleForm: FC<BoardTitleFormProps> = ({ board }) => {
+  const { execute } = useAction(updateBoard, {
+    onSuccess: (data) => {
+      toast.success(`Board "${data.title}" updated.`);
+      setTitle(data.title);
+      disabledEditing();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
   const formRef = useRef<ElementRef<"form">>(null);
   const inputRef = useRef<ElementRef<"input">>(null);
+
+  const [title, setTitle] = useState(board.title);
   const [isEditing, setIsEditing] = useState(false);
 
   const enableEditing = () => {
@@ -19,21 +35,38 @@ const BoardTitleForm: FC<BoardTitleFormProps> = ({ board }) => {
     setTimeout(() => {
       inputRef.current?.focus();
       inputRef.current?.select();
-    }, 50);
+    });
   };
 
   const disabledEditing = () => {
     setIsEditing(false);
   };
 
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({
+      id: board.id,
+      title,
+    });
+  };
+
+  const onBlur = () => {
+    formRef.current?.requestSubmit();
+  };
+
   if (isEditing) {
     return (
-      <form ref={formRef} className="flex items-center gap-x-2">
+      <form
+        action={onSubmit}
+        ref={formRef}
+        className="flex items-center gap-x-2"
+      >
         <FormInput
           id="title"
           ref={inputRef}
-          onBlur={() => {}}
-          defaultValue={board.title}
+          onBlur={onBlur}
+          defaultValue={title}
           className="text-lg font-bold px-[7px] py-1 h-7 bg-transparent focus-visible:outline-none focus-visible:ring-transparent border-none"
         />
       </form>
@@ -46,7 +79,7 @@ const BoardTitleForm: FC<BoardTitleFormProps> = ({ board }) => {
       variant={"transparent"}
       className="font-bold text-lg h-auto w-auto py-1 px-2"
     >
-      {board.title}
+      {title}
     </Button>
   );
 };
