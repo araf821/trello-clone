1586f33be9a13b95c4b430bcb475a69511d9f409
araf821 <araf.ahmed200@gmail.com>
2023-12-17 23:42:48 -0500

    feat: init board options popover component

--- app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardNavbar.tsx
@@ -1,6 +1,7 @@
 import { Board } from "@prisma/client";
 import { FC } from "react";
 import BoardTitleForm from "./BoardTitleForm";
+import BoardOptions from "./BoardOptions";
 
 interface BoardNavbarProps {
   board: Board;
@@ -10,6 +11,9 @@ const BoardNavbar: FC<BoardNavbarProps> = async ({ board }) => {
   return (
     <div className="w-full h-14 z-40 bg-black/50 fixed top-14 flex items-center px-6 gap-x-4 text-white">
       <BoardTitleForm board={board} />
+      <div className="ml-auto">
+        <BoardOptions id={board.id} />
+      </div>
     </div>
   );
 };
---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/BoardOptions.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { MoreHorizontal, X } from "lucide-react";
+import { FC } from "react";
+
+interface BoardOptionsProps {
+  id: string;
+}
+
+const BoardOptions: FC<BoardOptionsProps> = ({ id }) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-auto w-auto p-2" variant="transparent">
+          <MoreHorizontal className="h-5 w-5" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="px-0 py-3" side="bottom" align="start">
+        <div className="text-sm font-medium text-center text-neutral-600 pb-4">
+          Board actions
+        </div>
+        <PopoverClose asChild>
+          <Button
+            className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
+            variant="ghost"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <Button
+          onClick={() => {}}
+          className="rounded-none w-full h-auto py-2 font-normal text-sm px-5 justify-start"
+          variant="ghost"
+        >
+          Delete this board
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default BoardOptions;
