42bced5f6ebed9e4b8e6c1c564c92e8e7334f5ef
araf821 <araf.ahmed200@gmail.com>
2023-12-20 00:12:03 -0500

    feat: implement delete list functionality

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListOptions.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { deleteList } from "@/actions/delete-list";
 import FormSubmit from "@/components/form/FormSubmit";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,9 +10,11 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
+import { useAction } from "@/hooks/useAction";
 import { List } from "@prisma/client";
 import { MoreHorizontal, X } from "lucide-react";
-import { FC } from "react";
+import { ElementRef, FC, useRef } from "react";
+import { toast } from "sonner";
 
 interface ListOptionsProps {
   onAddCard: () => void;
@@ -19,6 +22,28 @@ interface ListOptionsProps {
 }
 
 const ListOptions: FC<ListOptionsProps> = ({ data, onAddCard }) => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+
+  const { execute: executeDeleteList } = useAction(deleteList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" was deleted.`);
+      closeRef.current?.click();
+    },
+    onError: (e) => {
+      toast.error(e);
+    },
+  });
+
+  const onDelete = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeDeleteList({
+      id,
+      boardId,
+    });
+  };
+
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -30,7 +55,7 @@ const ListOptions: FC<ListOptionsProps> = ({ data, onAddCard }) => {
         <div className="text-sm font-medium text-center text-neutral-600 pb-4">
           List Actions
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
             variant="ghost"
@@ -56,7 +81,7 @@ const ListOptions: FC<ListOptionsProps> = ({ data, onAddCard }) => {
           </FormSubmit>
         </form>
         <Separator />
-        <form action="">
+        <form action={onDelete}>
           <input hidden id="id" name="id" value={data.id} />
           <input hidden id="boardId" name="boardId" value={data.boardId} />
           <FormSubmit
